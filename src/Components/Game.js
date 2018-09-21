5a8d460c8d0e0376c6159d8cb3fbfc8b01980b0f
GiadaSimonetti <giada.simonetti@made.com>
2018-09-21 15:30:27 +0100

    Define a Game compomnent with height, width and  mines

--- src/Components/Game.js
+++ src/Components/Game.js
@@ -3,10 +3,17 @@ import React, { Component } from 'react';
 import Board from './Board';
 
 export default class Game extends Component {
+    state = {
+    height: 8,
+    width: 8,
+    mines: 10
+  };
+
   render() {
-    return (
-      <div className="cell-wrapper">
-        < Board />
+      const { height, width, mines } = this.state;
+      return (
+      <div className="game">
+        <Board height={height} width={width} mines={mines} />
       </div>
     );
   }
