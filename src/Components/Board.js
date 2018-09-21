9454f7b21fa1f1964fed5502f286cced416d0cab
GiadaSimonetti <giada.simonetti@made.com>
2018-09-21 15:35:01 +0100

    Define Board component

--- src/Components/Board.js
+++ src/Components/Board.js
@@ -1,13 +1,36 @@
 import React, { Component } from 'react';
+import PropTypes from 'prop-types';
 // import './Board.css';
 import Cell from './Cell'
 
 export default class Board extends Component {
+    state = {
+      boardData: this.initBoardData(this.props.height, this.props.width, this.props.mines),
+      gameStatus: false,
+      mineCount: this.props.mines
+    };
   render() {
     return (
-      <div className="board-wrapper">
-        < Cell />
+      <div className="board">
+        <div className="game-info">
+          <span className="info">
+            mines: {this.state.mineCount}
+          </span>
+          <br />
+          <span className="info">
+            {this.state.gameStatus}
+          </span>
+        </div>
+        { this.renderBoard(this.state.boardData)}
       </div>
-    );
-  }
+      );
+    }
+}
+
+
+// Type checking With PropTypes
+Board.propTypes = {
+  height: PropTypes.number,
+  width: PropTypes.number,
+  mines: PropTypes.number,
 }
