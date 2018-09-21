13f3a095218809b76c3452ea7cac58e50d8d0147
GiadaSimonetti <giada.simonetti@made.com>
2018-09-21 15:31:11 +0100

    The Cell component renders each square

--- src/Components/Cell.js
+++ src/Components/Cell.js
@@ -1,12 +1,43 @@
 import React, { Component } from 'react';
+import PropTypes from 'prop-types';
 // import './Cell.css';
 
 export default class Cell extends Component {
-  render() {
-    return (
-      <div className="cell-wrapper">
-        Hi!
-      </div>
-    );
-  }
+    getValue() {
+        const {value} = this.props;
+
+        if (!value.isRevealed) {
+          return this.props.value.isFlagged ? "🚩" : null;
+        }
+        if (value.isMine) {
+          return "💣";
+        }
+        if (value.neighbour === 0) {
+          return null;
+        }
+        return value.neighbour;
+      }
+    render() {
+        const {value, onClick, cMenu} = this.props;
+        return (
+          <div
+            onClick={this.props.onClick}
+            onContextMenu={this.props.cMenu}
+          >
+          {this.getValue()}
+          </div>
+        );
+      }
+}
+
+// Type checking With PropTypes
+const cellItemShape = {
+  isRevealed: PropTypes.bool,
+  isMine: PropTypes.bool,
+  isFlagged: PropTypes.bool
+}
+Cell.propTypes = {
+  value: PropTypes.objectOf(PropTypes.shape(cellItemShape)),
+  onClick: PropTypes.func,
+  cMenu: PropTypes.func
 }
